32c2201905667419d29d3b00d8c24bb83d35d15f
TestingCorps <73398190+TestingCorps@users.noreply.github.com>
2021-02-27 11:50:50 -0500

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,47 @@
+var ball;
+var database;
+var position;
+
+function setup(){
+    createCanvas(500,500);
+    database = firebase.database();
+    ball = createSprite(250,250,10,10);
+    ball.shapeColor = "red";
+
+    var ballPosition = database.ref("ball/position");
+    ballPosition.on("value",readPos,showErr);
+}
+
+function draw(){
+    background("white");
+    if(keyDown(LEFT_ARROW)){
+        writePos(-1,0);
+    }
+    else if(keyDown(RIGHT_ARROW)){
+        writePos(1,0);
+    }
+    else if(keyDown(UP_ARROW)){
+        writePos(0,-1);
+    }
+    else if(keyDown(DOWN_ARROW)){
+        writePos(0,+1);
+    }
+    drawSprites();
+}
+
+function writePos(x,y){
+    database.ref("ball/position").set({
+        x:position.x+x,
+        y:position.y+y
+    })
+}
+
+function showErr(){
+    console.log("Error..")
+}
+
+function readPos(data){
+    position = data.val();
+    ball.x = position.x;
+    ball.y = position.y;
+}
